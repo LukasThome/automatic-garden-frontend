381766811ed460cb2cd56ef25c8e948e1aaa9f02
lukasthome <thome_lucas@live.com>
2023-11-28 00:34:50 -0300

    home apresentando valores fornecidos pela api

--- AutomaticGarden/Home.js
+++ AutomaticGarden/Home.js
@@ -1,22 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Button } from 'react-native';
+import axios from 'axios';
+
+
 
 
 const HomeScreen = ({ navigation }) => {
-   
-    const navigateToConfigurations = () => {
-      navigation.navigate('Configurations');
-    };
-  
+  const [leituras, setLeituras] = useState([]);
 
-return (
+  useEffect(() => {
+    // Quando o componente montar, faça a solicitação para obter as leituras
+    fetchLeituras();
+  }, []);
+
+  const fetchLeituras = async () => {
+    try {
+      // Faça a solicitação GET para obter as leituras usando o Axios
+      const response = await axios.get('http://localhost:8090/obter-leituras');
+
+      // Atualize o estado com os dados recebidos
+      setLeituras(response.data.leituras);
+    } catch (error) {
+      console.error('Erro ao obter leituras:', error);
+    }
+  };
+
+  const navigateToConfigurations = () => {
+    navigation.navigate('Configurations');
+  };
+
+  return (
     <View style={styles.container}>
-        
-        <View style={styles.container}>
-        {/* Button to go to configs */}
-        <Button title="Configurations" onPress={navigateToConfigurations} />
-      </View>
+      {/* Botão para ir para as configurações */}
+      <Button title="Configurations" onPress={navigateToConfigurations} />
 
+      {/* Exibir os dados da tabela de leituras */}
+      <View>
+        <Text>Leituras:</Text>
+        {leituras.map((leitura) => (
+          <View key={leitura.id}>
+            <Text>{`Valor: ${leitura.valor}, Timestamp: ${leitura.timestamp}`}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
@@ -25,20 +51,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-  },
-  
-  logo: {
-    width: 50,
-    height: 50,
-    marginRight: 10,
-  },
-  name: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  favoriteButtonText: {
-    color: 'white',
-    fontSize: 12,
   },
 });
 
